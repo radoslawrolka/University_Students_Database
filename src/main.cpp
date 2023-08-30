8b5dc4af4a49cad76b4ac94058a9537500f87530
Radosław Rolka <radoslaw.rolka@gmail.com>
2023-08-30 22:23:21 +0200

    class main: refractor

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,14 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <conio.h>
+#include <vector>
+#include <windows.h>
+#include <cstdlib>
 
 void greet();
 UserClient* login();
+
 
 int main() {
     greet();
@@ -22,44 +27,36 @@ int main() {
 
 void greet() {
     std::cout << "Welcome to the University Students Database." << std::endl;
-    std::string command;
-    while (command != "login" && command != "exit") {
-        std::cout << R"((Write "login" to login or "exit" to exit.))" << std::endl;
-        getline(std::cin, command);
-        if (command == "exit") {
-            exit(0);
-        }
-        else if (command != "login") {
-            std::cout << "Wrong command." << std::endl;
-        }
-    }
+    std::cout << "(Press any key to continue)" << std::endl;
+    getch();
 }
 
 UserClient* login() {
+    HANDLE hOut;
+    hOut = GetStdHandle( STD_OUTPUT_HANDLE );
     std::ifstream file;
     file.open("../resources/login.password");
     if (!file.is_open()) {
+        SetConsoleTextAttribute( hOut, FOREGROUND_RED );
         std::cout << "System Error (No user data file)" << std::endl;
         exit(1);
     }
     else {
-        int counter = 0;
+        struct userData {
+            std::string login;
+            std::string password;
+            std::string role;
+        };
+        std::vector<userData> loginData;
         std::string line;
-        while (std::getline(file, line)) {
-            counter++;
-        }
-        std::string data[counter][3];
-        counter = 0;
-        file.clear();
-        file.seekg(std::ios::beg);
         std::getline(file, line);
         while (std::getline(file, line)) {
             std::istringstream ss(line);
-            std::string field;
-            for (int i=0; i<3; i++) {
-                std::getline(ss, data[counter][i], ';');
-            }
-            counter++;
+            userData data;
+            std::getline(ss, data.login, ';');
+            std::getline(ss, data.password, ';');
+            std::getline(ss, data.role, ';');
+            loginData.push_back(data);
         }
         file.close();
         std::string login;
@@ -70,19 +67,21 @@ UserClient* login() {
             std::cout << "Password:";
             getline(std::cin, password);
 
-            for (int i = 0; i < counter; i++) {
-                if (login == data[i][0] && password == data[i][1]) {
-                    if (data[i][2] == "admin") {
-                        std::cout<<"Hello "<<login<<"! (admin account)"<<std::endl;
+            for (auto &data : loginData) {
+                if (login == data.login && password == data.password) {
+                    if (data.role == "admin") {
+                        std::cout<< "Hello " << login << "! (admin account)" <<std::endl;
                         return new AdminClient();
                     }
-                    else if (data[i][2] == "user") {
-                        std::cout<<"Hello "<<login<<"! (user account)"<<std::endl;
+                    else {
+                        std::cout<< "Hello " << login << "! (user account)"<<std::endl;
                         return new UserClient();
                     }
                 }
             }
+            SetConsoleTextAttribute( hOut, FOREGROUND_RED );
             std::cout << "Wrong login or password." << std::endl;
+            SetConsoleTextAttribute( hOut, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED );
             std::cout << "Press Y to try again or N to exit." << std::endl;
             std::getline(std::cin, line);
             if (line == "N" || line == "n") {
